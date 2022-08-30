42762ba9760700184e548d11ab2044e4d1fa7384
gesora96 <36685490+zahidhasanemran@users.noreply.github.com>
2022-08-30 14:55:34 +0600

    project setup and useQuery with data

---
+++ src/Posts.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { PostDetail } from "./PostDetail";
+const maxPostPage = 10;
+
+async function fetchPosts() {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+  );
+  return response.json();
+}
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  // replace with useQuery
+  const { data } = useQuery("posts", fetchPosts);
+
+  return (
+    <>
+      <ul>
+        {data?.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button disabled onClick={() => {}}>
+          Previous page
+        </button>
+        <span>Page {currentPage + 1}</span>
+        <button disabled onClick={() => {}}>
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}

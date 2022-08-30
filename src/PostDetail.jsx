c223f798f1ec3d1b54f9e95e8b30928deb8043e7
gesora96 <36685490+zahidhasanemran@users.noreply.github.com>
2022-08-30 17:14:12 +0600

    key inside array and post.id for cache

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,16 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { isError, data, isLoading } = useQuery(["comments", post?.id], () =>
+    fetchComments(post?.id)
+  );
+
+  if (isError) {
+    return <div>Something went wrong</div>;
+  }
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
 
   return (
     <>
@@ -31,7 +42,7 @@ export function PostDetail({ post }) {
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {data?.map((comment) => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
